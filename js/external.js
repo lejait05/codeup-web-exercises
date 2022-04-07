01494743ce0e6ed9453d5f73616dbe6c9b07b62d
Lineisha <lineishagibson@gmail.com>
2022-04-07 10:42:29 -0500

    external_js.html

---
+++ js/external.js
@@ -0,0 +1,4 @@
+console.log("Hello from external JavaScript!");
+alert("Welcome to my Website!");
+var userInput = prompt('What is your favorite color?:');
+alert('Great I also like the color '  +  userInput);
