b4d0cb12dd33f882329d394d002e45e4336ae0c6
Lineisha <lineishagibson@gmail.com>
2022-04-21 15:48:42 -0500

    forms.html

--- js/objects.js
+++ js/objects.js
@@ -47,11 +47,12 @@ sayHello();
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+     var shoppers = [
+         {name: 'Cameron', amount: 180},
+         {name: 'Ryan', amount: 250},
+         {name: 'George', amount: 320}
+     ];
+     if amount >= 200
 
     /** TODO:
      * Create an array of objects that represent books and store it in a
