12358b01d1b15a9690ee05fe244c3ccad91c0b28
Lineisha <lineishagibson@gmail.com>
2022-05-13 14:30:43 -0500

    work

---
+++ js/custom-js.js
@@ -0,0 +1,12 @@
+"use strict";
+$(document).ready(function (){
+// jQuery -select element with ID of 'pageLink' and set innerHTML using html() method
+//     ID Selector
+    $('#pageLink').html("<h1>Link to Somewhere</h1>");
+
+    // var contents = $("#list").html();
+    // alert(contents);
+var contents = $("#app").html();
+alert(contents);
+});
+
