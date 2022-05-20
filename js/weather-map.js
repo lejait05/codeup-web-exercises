2afcb31551af028255f37ecccfb8703ac38a933f
Lineisha <lineishagibson@gmail.com>
2022-05-20 10:57:11 -0500

    5-20

---
+++ js/weather-map.js
@@ -0,0 +1,22 @@
+// $.get("http://api.openweathermap.org/data/2.5/weather", {
+//     APPID: OPEN_WEATHER_APPID,
+//     q:     "San Antonio, US",
+//     units: "imperial"
+// }).done(function(data) {
+//     console.log(data);
+// });
+
+
+$.get("http://api.openweathermap.org/data/2.5/onecall", {
+    APPID: OPEN_WEATHER_APPID,
+    lat:    29.423017,
+    lon:   -98.48527,
+    units: "imperial"
+}).done(function(data) {
+    console.log('The entire response:', data);
+    console.log('Diving in - here is current information: ', data.current);
+    console.log('A step further - information for tomorrow: ', data.daily[1]);
+data.daily.forEach(function (dailyForcast, index){
+    if (index < 5 );
+})
+});
